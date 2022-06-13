b76655c1e1fe8faace52d827b682eccbfa81db01
weilin9999 <w@wltip.com>
2022-06-14 07:53:34 +0800

    更新视频流量功能 - Update video traffic function

--- routers/Router.go
+++ routers/Router.go
@@ -13,6 +13,7 @@ import (
 	"VideoHubGo/controllers/RelationController"
 	"VideoHubGo/controllers/UserController"
 	"VideoHubGo/controllers/VideoController"
+	"VideoHubGo/controllers/WatchController"
 	"VideoHubGo/middlewares/JwtMiddleware"
 	"VideoHubGo/middlewares/NoRouteMiddlewares"
 	"VideoHubGo/utils/LogUtils"
@@ -68,36 +69,46 @@ func Router(router *gin.Engine) *gin.Engine {
 		routerList2.POST("/upload/avatar", UserController.UploadAvatar)         //用户头像上传控制器 - User Upload Avatar Controller
 	}
 
-	//后台路由 - Admin Route
-	routerList3 := router.Group("/admin").Use(JwtMiddleware.JwtMiddleware()) // JWT中间件 - JWT Middleware
-	{
-		routerList3.POST("/login", AdminController.AdminLogin) //用户登录控制器 - User Login Controller
-	}
-
 	//视频分类路由 - Video Class Route
-	routerList4 := router.Group("/class") //.Use(JwtMiddleware.JwtMiddleware()) // JWT中间件 - JWT Middleware
+	routerList3 := router.Group("/class").Use(JwtMiddleware.JwtMiddleware()) // JWT中间件 - JWT Middleware
 	{
-		routerList4.POST("/list", ClassController.GetClassList) //视频控制器 - Video Controller
+		routerList3.POST("/list", ClassController.GetClassList) //视频控制器 - Video Controller
 	}
 
 	//视频路由 - Video Route
-	routerList5 := router.Group("/video") //.Use(JwtMiddleware.JwtMiddleware()) // JWT中间件 - JWT Middleware
+	routerList4 := router.Group("/video").Use(JwtMiddleware.JwtMiddleware()) // JWT中间件 - JWT Middleware
 	{
-		routerList5.POST("/list", VideoController.GetVideoList)             //总视频控制器 - Center Video Controller
-		routerList5.POST("/class/list", VideoController.GetVideoClassList)  //视频类型控制器 - Video Class Controller
-		routerList5.POST("/search/list", VideoController.SearchVideo_Class) //视频搜索控制器 - Video Search Controller
+		routerList4.POST("/list", VideoController.GetVideoList)                //总视频控制器 - Center Video Controller
+		routerList4.POST("/class/list", VideoController.GetVideoClassList)     //视频类型控制器 - Video Class Controller
+		routerList4.POST("/search/list", VideoController.SearchVideoClassList) //视频搜索控制器 - Video Search Controller
 	}
 
 	//用户收藏路由 - Relation Route
-	routerList6 := router.Group("/relation").Use(JwtMiddleware.JwtMiddleware()) // JWT中间件 - JWT Middleware
+	routerList5 := router.Group("/relation").Use(JwtMiddleware.JwtMiddleware()) // JWT中间件 - JWT Middleware
 	{
-		routerList6.POST("/list", RelationController.GetRelationList) //总收藏控制器 - Center Relation Controller
+		routerList5.POST("/list", RelationController.GetRelationList)                //总收藏控制器 - Center Relation Controller
+		routerList5.POST("/class/list", RelationController.FindRelationClassList)    //搜藏类型控制器 - Relation Class Controller
+		routerList5.POST("/search/list", RelationController.SearchRelationClassList) //收藏搜索控制器 - Relation Search Controller
+		routerList5.POST("/add", RelationController.RelationVideo)                   //添加用户收藏 - Add User Relation
+		routerList5.POST("/delete", RelationController.RemoveRelation)               //取消用户收藏 - Delete User Relation
 	}
 
+	//视频详细路由 - Video Detail Route
+	routerList6 := router.Group("/watch").Use(JwtMiddleware.JwtMiddleware()) // JWT中间件 - JWT Middleware
+	{
+		routerList6.POST("find", WatchController.GetVideoDetail) //获取视频详细信息 - Query video details
+		routerList6.POST("plus", WatchController.PlusVideoWatch) //增加视频流量 - Increase video traffic
+	}
 	//文件映射 - Map File
-	routerList7 := router.Group("/file")
+	routerList7 := router.Group("/file").Use(JwtMiddleware.JwtMiddleware())
 	{
 		routerList7.Static("/avatar", UploadUtils.GetUploadFilePath("user.userAvatar")) //映射头像文件夹 - Map avatar folder
+	}
+
+	//后台路由 - Admin Route
+	routerList8 := router.Group("/admin")
+	{
+		routerList8.POST("/login", AdminController.AdminLogin) //用户登录控制器 - User Login Controller
 	}
 
 	return router
